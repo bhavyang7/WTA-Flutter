26e68405e44403c9b279cd477bb6b1cbbd2593c5
bhavyang7 <39518761+bhavyang7@users.noreply.github.com>
2020-06-23 22:49:38 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:worldtime/pages/home.dart';
+import 'package:worldtime/pages/loading.dart';
+import 'package:worldtime/pages/choose.dart';
+
+void main() => runApp(MaterialApp(
+  initialRoute: '/home',
+  routes: {
+    '/': (context) => Loading(),
+    '/home': (context) => Home(),
+    '/choose': (context)=> Choose(),
+  },
+));
+
